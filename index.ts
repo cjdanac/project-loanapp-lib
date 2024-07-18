8f3a24b8c363ff04b6f7efb7d101a5e74ecc5d9f
Christine Joy DC. Anac <cjanac@TELIGENT.PH>
2024-07-18 16:07:30 +0800

    removeDATA

--- index.ts
+++ index.ts
@@ -1,7 +1,6 @@
 import { Environment, EnvProps } from "./src/configs";
 import { GetDataModel } from "./src/model/data/DataModel";
 import { PaymentModel } from "./src/model/payment/PaymentModel";
-import { getData } from "./src/request/data/dataRequest";
 import { payment } from "./src/request/payment/paymentRequest";
 
 export {
@@ -17,7 +16,6 @@ export {
 }
 
 export {
-    getData,
     payment,
     
 }
