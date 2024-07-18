99ccf4596b89a847d8cb31af2610ba16e2b2d947
Christine Joy DC. Anac <cjanac@TELIGENT.PH>
2024-07-18 16:17:32 +0800

    removeDATA

--- src/configs/EnvProps.ts
+++ src/configs/EnvProps.ts
@@ -3,5 +3,4 @@ export type EnvProps = {
     API_URL: string,
     INTEREST: string,
     PAYMENT: string,
-    DATA: string,
   };
--- src/configs/index.ts
+++ src/configs/index.ts
@@ -3,14 +3,12 @@ import { EnvProps } from "./EnvProps"
 class Environment {
     static API_URL: string;
     static INTEREST: string;
-    static DATA: string;
     static PAYMENT: string;
 
   static init(env: EnvProps) {
       this.API_URL = env.API_URL;
       this.INTEREST = env.INTEREST;
-      this.DATA = env.DATA;
-      this.PAYMENT = env.PAYMENT;
+      this.PAYMENT = env.PAYMENT
   }
   
 }
