92fe08ec971e756d861aa00d879a75a1ff85ffec
Christine Joy DC. Anac <cjanac@TELIGENT.PH>
2024-07-22 15:41:31 +0800

    updateAPIInstance

--- src/services/apiInstance.ts
+++ src/services/apiInstance.ts
@@ -1,19 +1,25 @@
-
 import axios, { AxiosError, AxiosInstance } from "axios";
 import { Environment } from "../configs";
 
+ 
 const apiInstance = axios.create({
   baseURL: Environment.API_URL,
+  headers: {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  },
 });
-
+ 
 apiInstance.interceptors.response.use(
   function (response) {
     return response;
   },
   async function (error) {
-    
+    if (!apiInstance.defaults.baseURL) {
+      apiInstance.defaults.baseURL = Environment.API_URL;
+    }
     return Promise.reject(error);
   }
 );
-
-export { apiInstance};
+ 
+export { apiInstance };
