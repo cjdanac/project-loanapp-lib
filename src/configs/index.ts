4f6487d451b9cc14768891712077b9a00dc440db
Christine Joy DC. Anac <cjanac@TELIGENT.PH>
2024-07-22 15:04:57 +0800

    updateLoanEnvProps

--- src/configs/index.ts
+++ src/configs/index.ts
@@ -1,8 +1,8 @@
 import { EnvProps } from "./EnvProps"
 
 class Environment {
-    static API_URL: string;
-    static LOAN: string;
+    static API_URL?: string;
+    static LOAN?: string;
 
   static init(env: EnvProps) {
       this.API_URL = env.API_URL;
